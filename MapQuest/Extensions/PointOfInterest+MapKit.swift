5ea622add052379fe0822a07bef7f0fc6f68ae4f
Chef Pops <ddixonjr@gmail.com>
2021-02-12 23:12:33 -0500

    Deleted more raywinderlich trash

--- MapQuest/Extensions/PointOfInterest+MapKit.swift
+++
@@ -1,9 +0,0 @@
-
-
-import Foundation
-import MapKit
-
-extension PointOfInterest: MKAnnotation {
-  var coordinate: CLLocationCoordinate2D { return location.coordinate }
-  var title: String? { return name }
-}
