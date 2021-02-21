8cd8926c58c06fe4bd1e6032b152bb1eb1f0a30b
Thomas Deng <thomasxdeng@gmail.com>
2021-02-20 20:45:55 -0800

    Server added

--- MapQuest/MapViewController.swift
+++ MapQuest/MapViewController.swift
@@ -5,13 +5,39 @@ import MapKit
 import WebKit
 class MapViewController: UIViewController {
   // MARK: - IBOutlets
-  
+  func post(){
+    let url = URL(string: "https://jsonplaceholder.typicode.com/todos")
+    guard let requestUrl = url else { fatalError() }
+    // Prepare URL Request Object
+    var request = URLRequest(url: requestUrl)
+    request.httpMethod = "POST"
+     
+    // HTTP Request Parameters which will be sent in HTTP Request Body
+    let postString = "userId=300&title=My urgent task&completed=false";
+    // Set HTTP Request Body
+    request.httpBody = postString.data(using: String.Encoding.utf8);
+    // Perform HTTP Request
+    let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+            
+            // Check for Error
+            if let error = error {
+                print("Error took place \(error)")
+                return
+            }
+     
+            // Convert HTTP Response Data to a String
+            if let data = data, let dataString = String(data: data, encoding: .utf8) {
+                print("Response data string:\n \(dataString)")
+            }
+    }
+    task.resume()
+  }
   var previous: MKAnnotation?
   
   var speed: Double = 100000
 
   var warps: WarpZone =  WarpZone(latitude: 45.76518, longitude: -73.990)
-
+  
   @IBOutlet weak var mapView: MKMapView!
   @IBOutlet weak var heartsLabel: UILabel!
   
@@ -35,8 +61,9 @@ class MapViewController: UIViewController {
     
     
   }
-  func updatePos(_ annotation: MKPointAnnotation){
   
+  func updatePos(_ annotation: MKPointAnnotation){
+  post()
   let latitude = annotation.coordinate.latitude
   let longitude = annotation.coordinate.longitude
     let deltax = latitude - warps.coordinate.latitude
