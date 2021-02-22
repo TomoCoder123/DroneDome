dccee261d0919a36a5158d8dc8968a864b1d1cb8
CoolCoder54323 <nicxellent@icloud.com>
2021-02-22 02:50:28 -0500

    Fixed server relations, and added extra zoom layer

--- MapQuest/MapViewController.swift
+++ MapQuest/MapViewController.swift
@@ -3,19 +3,25 @@
 import UIKit
 import MapKit
 import WebKit
-class MapViewController: UIViewController {
+public class MapViewController: UIViewController {
   // MARK: - IBOutlets
-  func post(){
-    let url = URL(string: "https://jsonplaceholder.typicode.com/todos")
+//  let timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateDrone(_:)), userInfo: nil, repeats: true)
+  
+  var globX: Double = -63.754138814196
+  var globY: Double = 13.9317203835678
+
+  func post(xcord x: Double,ycord y: Double){
+    let url = URL(string: "http://192.168.4.27:5000/requester")
     guard let requestUrl = url else { fatalError() }
     // Prepare URL Request Object
     var request = URLRequest(url: requestUrl)
     request.httpMethod = "POST"
      
     // HTTP Request Parameters which will be sent in HTTP Request Body
-    let postString = "userId=300&title=My urgent task&completed=false";
+    let postString = "xpos=\(x)&ypos=\(y)";
     // Set HTTP Request Body
     request.httpBody = postString.data(using: String.Encoding.utf8);
+    request.setValue("application/json", forHTTPHeaderField: "Accept")
     // Perform HTTP Request
     let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
             
@@ -31,6 +37,64 @@ class MapViewController: UIViewController {
             }
     }
     task.resume()
+  }
+  
+  @objc public func updateDrone() {
+    var arr = getPos()
+    arr[0] = self.globX
+    arr[1] = self.globY
+    print("\(arr) my reason...")
+    updatePos(longitude: arr[0], latitude: arr[1])
+    repeatUpdateDrone()
+  }
+  
+  func getPos() -> [Double] {
+    
+    var position = [0.0,0.0]
+    // Create URL
+    let url = URL(string: "http://192.168.4.27:5000/requester")
+    guard let requestUrl = url else { fatalError() }
+    // Create URL Request
+    var request = URLRequest(url: requestUrl)
+    // Specify HTTP Method to use
+    request.httpMethod = "GET"
+    // Send HTTP Request
+    let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+        
+        // Check if Error took place
+        if let error = error {
+            print("Error took place \(error)")
+            return
+        }
+        
+        // Read HTTP Response Status code
+        if let response = response as? HTTPURLResponse {
+            print("Response HTTP Status code: \(response.statusCode)")
+        }
+        
+        // Convert HTTP Response Data to a simple String
+        if let data = data, let dataString = String(data: data, encoding: .utf8) {
+            print("Response data string:\n \(dataString)")
+        }
+        do {
+            if let convertedJsonIntoDict = try JSONSerialization.jsonObject(with: data!, options: []) as? NSDictionary {
+                  // Print out entire dictionary
+                  print(convertedJsonIntoDict["x"] as! Double)
+                  // Get value by key
+              self.globX = convertedJsonIntoDict["x"] as! Double
+              self.globY = convertedJsonIntoDict["y"] as! Double
+              print("\(self.globY) this is my 13th reason")
+                     
+                 }
+              } catch let error as NSError {
+                 print(error.localizedDescription)
+       }
+
+      }
+    print(position)
+
+    task.resume()
+    return position
   }
   var previous: MKAnnotation?
   
@@ -55,24 +119,23 @@ class MapViewController: UIViewController {
     annotation.title = "Drone Destination"
     annotation.subtitle = "Location of Destination"
     self.mapView.addAnnotation(annotation)
-    
-    updatePos(longitude: annotation.coordinate.longitude, latitude: annotation.coordinate.latitude)
+    self.post(xcord: annotation.coordinate.longitude, ycord: annotation.coordinate.latitude)
+    //updatePos(longitude: annotation.coordinate.longitude, latitude: annotation.coordinate.latitude)
    
     
     
   }
   
   func updatePos(longitude: Double, latitude: Double){
-  post()
    
-    let deltax = latitude - 10 - warps.coordinate.latitude
+    let deltax = latitude - warps.coordinate.latitude
     let deltay = longitude - warps.coordinate.longitude
-    print(latitude)
-    print(longitude)
-    print(warps.coordinate.latitude)
-    print(warps.coordinate.longitude)
+//    print(latitude)
+//    print(longitude)
+//    print(warps.coordinate.latitude)
+//    print(warps.coordinate.longitude)
 
-    UIView.animate(withDuration: 10){
+    UIView.animate(withDuration: 1){
       self.warps.coordinate.latitude += deltax
       self.warps.coordinate.longitude += deltay
     }
@@ -95,12 +158,12 @@ class MapViewController: UIViewController {
     warps = WarpZone(latitude: 45.76518, longitude: -73.990)
     
   }
-  override func viewDidLoad() {
+  override public func viewDidLoad() {
     super.viewDidLoad()
     setupWarps()
     setupTileRenderer()
     WebView.scrollView.isScrollEnabled = false;
-    let urls = URL(string: "http://192.168.56.1:6006/")!;
+    let urls = URL(string: "http://192.168.4.27:5000/video_feed")!;
     WebView.load(URLRequest(url: urls))
     let initialRegion = MKCoordinateRegion(
       center: CLLocationCoordinate2D(latitude: 0, longitude: 0),
@@ -122,10 +185,17 @@ class MapViewController: UIViewController {
     mapView.delegate = self
   }
 
-  override func viewWillAppear(_ animated: Bool) {
-    super.viewWillAppear(animated)
+  override public func viewDidAppear(_ animated: Bool) {
+    super.viewDidAppear(animated)
+    repeatUpdateDrone()
   }
 
+  func repeatUpdateDrone() {
+    DispatchQueue.main.asyncAfter(deadline: .now() + 1.0, execute: { [weak self] in
+      self?.updateDrone()
+    })
+
+  }
   private func setupTileRenderer() {
     let overlay = AdventureMapOverlay()
 
@@ -134,17 +204,17 @@ class MapViewController: UIViewController {
     tileRenderer = MKTileOverlayRenderer(tileOverlay: overlay)
 
     overlay.minimumZ = 3
-    overlay.maximumZ = 4
+    overlay.maximumZ = 5
   }
 
-  @objc func gameUpdated(notification: Notification) {
+  @objc public func gameUpdated(notification: Notification) {
   }
 }
 
 // MARK: - MKMapViewDelegate
 extension MapViewController: MKMapViewDelegate {
   // Add delegates here
-  func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+  public func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
     if overlay is AdventureMapOverlay {
       return tileRenderer
     } else {
@@ -152,7 +222,7 @@ extension MapViewController: MKMapViewDelegate {
     }
   }
 
-  func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+  public func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
 //    self.coordinate = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
     switch annotation {
     
